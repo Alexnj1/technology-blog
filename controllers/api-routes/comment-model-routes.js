984df431778748012f18b3d1f43a029180170ca3
Alex Noble-James <alexnoble143.an@gmail.com>
2022-03-16 16:00:25 -0400

    added comment api routes

---
+++ controllers/api-routes/comment-model-routes.js
@@ -0,0 +1,106 @@
+const { Post, User, Comment } = require("../../models");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Comment.findAll({
+    attributes: ["id", "text"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Post,
+        attributes: ["title"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+      console.log(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Comment.findOne({
+    attributes: ["id","text"],
+    include: {
+      model: Post,
+      attributes: ["title"],
+      include: {
+        model: User,
+        attributes: ["username"],
+      },
+    },
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+      console.log(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Comment.create({
+    text: req.body.text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400);
+      console.log(err).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Comment.update(
+    {
+      text: req.body.text,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then(() => {
+      res.status(200).json({Status: 'Success!'});
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+      console.log(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      res.status(200).json({Status: 'Success!'});
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+      console.log(err);
+    });
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,16 +1,29 @@
-const User = require('./User')
-const Post = require('./Post')
-// const Comment = require('./Comment')
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-})
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
-})
+  foreignKey: "user_id",
+});
 
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
-module.exports =  {User, Post, 
-    // Comment
-}
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
