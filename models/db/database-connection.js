61813881618c373fa325cf1dfdcfeff778fa9ffd
Alex Noble-James <alexnoble143.an@gmail.com>
2022-03-21 19:33:06 -0400

    d

--- models/db/database-connection.js
+++ models/db/database-connection.js
@@ -1,30 +1,17 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-require("dotenv").config();
+require('dotenv').config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL, {
-    pool: {
-      max: 100,
-      min: 0,
-      idle: 200000,
-      // @note https://github.com/sequelize/sequelize/issues/8133#issuecomment-359993057
-      acquire: 1000000,
-    },
-  });
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 }
 
 module.exports = sequelize;
